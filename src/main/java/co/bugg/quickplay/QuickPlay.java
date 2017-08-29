d0015450ab9d046c06c2e0896290e8c74efc1695
bugfr <bugfroggy@gmail.com>
2017-08-29 19:52:57 -0400

    removed 1.11 support (it dont work dood)

--- src/main/java/co/bugg/quickplay/QuickPlay.java
+++ src/main/java/co/bugg/quickplay/QuickPlay.java
@@ -17,7 +17,7 @@ import java.util.HashMap;
         modid = Reference.MOD_ID,
         name = Reference.MOD_NAME,
         version = Reference.VERSION,
-        acceptedMinecraftVersions = "[1.11,1.12.1]",
+        acceptedMinecraftVersions = "[1.12,1.12.1]",
         clientSideOnly = true,
         updateJSON = "https://raw.githubusercontent.com/bugfroggy/QuickPlay/master/versions.json"
 )
