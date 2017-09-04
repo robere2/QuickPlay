cf529d93198bbcf791aa064cbfe32e557fe7c312
bugfr <bugfroggy@gmail.com>
2017-09-03 22:36:49 -0400

    Updated Util class to contain more methods for the /qpdebug command

--- src/main/java/co/bugg/quickplay/Util.java
+++ src/main/java/co/bugg/quickplay/Util.java
@@ -1,6 +1,12 @@
 package co.bugg.quickplay;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ServerData;
 import net.minecraftforge.common.ForgeVersion;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * Miscellaneous Utilities
@@ -11,20 +17,105 @@ public class Util {
      * @return String version number
      */
     public static String getMCVersion() {
-        String defaultVersion = "1.12.1";
+        return getForgeVersionField("mcVersion", "Unknown");
+    }
+
+    /**
+     * Get the version of MCP this client is using
+     * @return String version number
+     */
+    public static String getMCPVersion() {
+        return getForgeVersionField("mcpVersion", "Unknown");
+    }
+
+    public static String getForgeVersion() {
+        String defaultValue = "Unknown";
         try {
-            String version = ForgeVersion.class.getField("mcVersion").get("mcVersion").toString();
-            if(version != null) {
-                return version;
-            } else {
-                return defaultVersion;
+            Method versionMethod = getMethod(ForgeVersion.class, "getVersion");
+
+            if(versionMethod == null) {
+                return defaultValue;
             }
-        } catch (NoSuchFieldException e) {
+
+            return (String) versionMethod.invoke(null);
+
+        } catch (IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
-            return defaultVersion;
+            return defaultValue;
+        }
+    }
+
+    /**
+     * Get a field value from the ForgeVersion class
+     * @param fieldName Field to grab
+     * @param defaultValue Value to return if the field isn't defined
+     * @return Field value, or default in case of undefined
+     */
+    public static String getForgeVersionField(String fieldName, String defaultValue) {
+        try {
+            Field valueField = getField(ForgeVersion.class, fieldName);
+
+            if(valueField == null) {
+                return defaultValue;
+            }
+
+            return (String) valueField.get(null);
+
         } catch (IllegalAccessException e) {
             e.printStackTrace();
-            return defaultVersion;
+            return defaultValue;
+        }
+    }
+
+    /**
+     * Get the IP of the current server the player is on
+     * @return IP, N/A if single player, or null if not on server
+     */
+    public static String getIP() {
+        boolean singleplayer = Minecraft.getMinecraft().isSingleplayer();
+        String ip;
+
+        if(singleplayer) {
+            ip = "N/A";
+        } else {
+            ServerData serverData = Minecraft.getMinecraft().getCurrentServerData();
+            ip = (serverData == null) ? null : serverData.serverIP;
+        }
+
+        return ip;
+    }
+
+    /**
+     * Get the field of a class reflectively at runtime.
+     * @param theClass The class to get the field from
+     * @param fieldName The field to get
+     * @return Field that was requested
+     */
+    public static Field getField(Class<?> theClass, String fieldName) {
+        try {
+            Field field = theClass.getField(fieldName);
+            field.setAccessible(true);
+            return field;
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Get the method of a class reflectively at runtime.
+     * @param theClass The class to get the field from
+     * @param methodName The method to get
+     * @return Method that was requested
+     */
+    public static Method getMethod(Class<?> theClass, String methodName, Class<?>... args) {
+        try {
+            Method method = theClass.getMethod(methodName, args);
+            method.setAccessible(true);
+            return method;
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
