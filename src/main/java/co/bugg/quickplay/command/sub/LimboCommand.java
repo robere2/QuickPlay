9e22da3182f74ffded98584f49cd854c6709d722
bugfr <bugfroggy@gmail.com>
2017-09-28 15:29:33 -0400

    Fixed /qp limbo in guild chat, added vampirez

--- src/main/java/co/bugg/quickplay/command/sub/LimboCommand.java
+++ src/main/java/co/bugg/quickplay/command/sub/LimboCommand.java
@@ -20,7 +20,7 @@ public class LimboCommand extends QpSubCommand {
         if(QuickPlay.onHypixel) {
             // Sending this chat message will kick the player, therefore
             // sending them to limbo.
-            Minecraft.getMinecraft().thePlayer.sendChatMessage("§");
+            Minecraft.getMinecraft().thePlayer.sendChatMessage("/achat §");
         } else {
             sender.addChatMessage(new TextComponentTranslation("quickplay.command.not_on_hypixel").setStyle(new Style().setColor(TextFormatting.RED)));
         }
