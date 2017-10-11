c21bb1b4e8601626ba85aaf210d2d4691d71a7d4
bugfroggy <bugfroggy@gmail.com>
2017-10-11 01:50:43 -0400

    added todo

--- src/main/java/co/bugg/quickplay/config/ConfigList.java
+++ src/main/java/co/bugg/quickplay/config/ConfigList.java
@@ -167,6 +167,7 @@ public class ConfigList extends AbstractConfigList{
 
                         break;
 
+                        // TODO: Implement keybinds here!
                     case KEYBIND:
 
                         break;
