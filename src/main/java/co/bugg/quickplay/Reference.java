a3ea9612022798441132cbb9bd9a969c251cdccc
bugfroggy <bugfroggy@gmail.com>
2018-01-11 16:27:46 -0500

    Updated version

--- src/main/java/co/bugg/quickplay/Reference.java
+++ src/main/java/co/bugg/quickplay/Reference.java
@@ -3,7 +3,7 @@ package co.bugg.quickplay;
 public class Reference {
     public static final String MOD_ID = "quickplay";
     public static final String MOD_NAME = "QuickPlay";
-    public static final String VERSION = "1.2.7";
+    public static final String VERSION = "1.2.8";
     public static final String COMPATIBLE_MC_VERSION_MIN = "1.12";
     public static final String COMPATIBLE_MC_VERSION_MAX = "1.12.2";
     public static final String ACCEPTED_VERSIONS = "[1.12, 1.12.2]";
